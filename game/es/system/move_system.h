fe586eddb68b8b0edb42e5ac9732637c9139b50a
Wim Van de Meerssche <wim.vandemeerssche@intec.ugent.be>
2022-11-02 10:22:00 +0100

    Fix RELATIVE8F_X, RELATIVE8F_Y en PLAYER_MOVE_MS

--- game/es/system/move_system.h
+++ game/es/system/move_system.h
@@ -3,10 +3,7 @@
 
 #include "../engine.h"
 
-#define OBJECT_FALL_MS 10u
-#define OBJECT_FALL_DIST 0.05f
-#define PLAYER_MOVE_MS 2000u
-#define DOOR_MOVE_MS 200u
+#define PLAYER_MOVE_MS 500u
 
 int do_blockers_overlap__excluding_edge(Engine* engine, EntityId a, EntityId b);
 int is_point_in_blocker__excluding_edge(Engine* engine, t_vec3 point, EntityId a);
